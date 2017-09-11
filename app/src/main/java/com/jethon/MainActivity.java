46c6f05fd416e6e9d312d46f6879dca303fa18b2
fand <dong.fan@topplusvision.com>
2017-09-11 11:52:52 +0800

    [fand]初始化

--- app/src/main/java/com/jethon/MainActivity.java
+++ app/src/main/java/com/jethon/MainActivity.java
@@ -59,5 +59,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-
+    @Override
+    protected void onDestroy() {
+        //资源回收
+        RxHttp.release();
+        super.onDestroy();
+    }
 }
